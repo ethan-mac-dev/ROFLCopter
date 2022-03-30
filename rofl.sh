246e200d4feed321b011ac56cd538ce720238956
Ethan <ethan.mcintyre@protonmail.com>
2022-03-30 14:56:37 +0100

    Fix animation starting early and x pipefail breakage

--- rofl.sh
+++ rofl.sh
@@ -1,13 +1,13 @@
 #/bin/bash
 
-set -euxo pipefail
+set -euo pipefail
 
 while :
 do
+  clear
   cat /path/to/ROFL/frame1
   sleep 0.3
   clear
   cat /path/to/ROFL/frame2
   sleep 0.3
-  clear
 done
